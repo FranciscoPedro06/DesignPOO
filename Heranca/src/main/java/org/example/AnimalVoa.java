68e95d17c81eea25226561264bae1b292dfb59ad
Francisco Pedro <pedro2006francisco@gmail.com>
2025-08-21 14:44:11 -0300

    Delete Heranca directory

--- Heranca/src/main/java/org/example/AnimalVoa.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class AnimalVoa extends Animal {
-    public AnimalVoa(String nome, String raca, String cor, int idade) {
-        super(nome, raca, cor, idade);
-    }
-}
